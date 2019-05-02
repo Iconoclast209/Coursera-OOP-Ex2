c342e5bc356c7c0958ecde97ed48dde88c890d0d
Anthony Smith <mrpresidentsmith@gmail.com>
2019-05-02 09:38:20 -0700

    Assignement completed.

--- UsingTextIO/Program.cs
+++ UsingTextIO/Program.cs
@@ -7,10 +7,30 @@ namespace UsingTextIO
     {
         static void Main(string[] args)
         {
-            
+            StreamReader input = null;
+            try
+            {
+                input = File.OpenText("quotes.txt");
 
+                string line = input.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = input.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
-
+            finally
+            {
+                if (input != null)
+                {
+                    input.Close();
+                }
+            }
         }
     }
 }
